c7325f5e799d5ad25a415e5af681efc94207423d
François de Metz <francois@2metz.fr>
2024-04-27 15:35:19 +0200

    Add popup documentation.

--- lib/components/popup.component.ts
+++ lib/components/popup.component.ts
@@ -18,9 +18,23 @@ import {
 import { MapLib } from "@/lib/lib/map.lib";
 import { mapSymbol, markerSymbol } from "@/lib/types";
 
-export default /*#__PURE__*/ defineComponent({
+/**
+ * A popup component
+ *
+ * See [Popup](https://maplibre.org/maplibre-gl-js/docs/API/classes/Popup/).
+ */
+export default defineComponent({
   name: "MglPopup",
-  emits: ["open", "close"],
+  emits: [
+    /**
+     * Fired when the popup is opened manually or programmatically.
+     */
+    "open",
+    /**
+     * Fired when the popup is closed manually or programmatically.
+     */
+    "close"
+  ],
   props: {
     /**
      * The geographical location of the popup's anchor.
@@ -31,44 +45,74 @@ export default /*#__PURE__*/ defineComponent({
       required: false,
     },
     /**
-     * Display a close button in the top right corner
+     * Display a close button in the top right corner.
      */
     closeButton: {
       type: Boolean,
       required: false,
       default: true,
     },
+    /**
+     * The popup will be closed when the map is clicked.
+     */
     closeOnClick: {
       type: Boolean,
       required: false,
       default: true,
     },
+    /**
+     * The popup will be closed when the map moves.
+     */
     closeOnMove: {
       type: Boolean,
       required: false,
       default: false,
     },
+    /**
+     * The popup will try to focus the first focusable element inside the popup.
+     */
     focusAfterOpen: {
       type: Boolean,
       required: false,
       default: true,
     },
+    /**
+     * A string indicating the part of the Popup that should
+     * be positioned closest to the coordinate.
+     * Options are `'center'`, `'top'`, `'bottom'`, `'left'`, `'right'`, `'top-left'`,
+     * `'top-right'`, `'bottom-left'`, and `'bottom-right'`. If unset the anchor will be
+     * dynamically set to ensure the popup falls within the map container with a preference
+     * for `'bottom'`.
+     */
     anchor: {
       type: String as PropType<PositionAnchor>,
       required: false,
     },
+    /**
+     * A pixel offset applied to the popup's location
+     */
     offset: {
       type: [Number, Object, Array] as PropType<Offset>,
       required: false,
     },
+    /**
+     * Space-separated CSS class names to add to popup container
+     */
     className: {
       type: String,
       required: false,
     },
+    /**
+     * A string that sets the CSS property of the popup's maximum width, eg `'300px'`.
+     * To ensure the popup resizes to fit its content, set this property to `'none'`.
+     */
     maxWidth: {
       type: String,
       default: "240px",
     },
+    /**
+     * Sets the popup's content to a string of text.
+     */
     text: {
       type: String,
       required: false,
