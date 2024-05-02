aab0b7d309e8ef30c6244d6e8b43d8fcd8f941ad
François de Metz <francois@2metz.fr>
2024-05-02 10:44:44 +0200

    Reformat the components documentation code.

--- docs/api/components.data.js
+++ docs/api/components.data.js
@@ -1,5 +1,62 @@
 import { parse } from 'vue-docgen-api';
 
+function formatProps(props) {
+  return props ? `
+## Props
+
+| Name | Description | Type | Required | Default value |
+|------|-------------|------|----------|---------------|
+${props.map((prop) => {
+  return [prop.name, prop?.description?.replaceAll('\n', '  '), prop.type?.name, prop.required || false, prop.defaultValue?.value].join('|')
+ }).join('\n')}
+` : '';
+}
+
+function formatEvents(events) {
+  return events ? `
+## Events
+
+| Name | Description | Properties |
+|------|-------------|------------|
+${events.map((event) => {
+  return [event.name, event.description, event.type].join('|')
+ }).join('\n')}
+` : '';
+}
+
+const formatBindings = (bindings) => {
+  if (!bindings) {
+    return ''
+  }
+  return bindings
+    .map(binding => {
+      const { name, description, type } = binding
+      if (!type) {
+	return ''
+      }
+      return `**${name}** \`${
+	type.name === 'union' && type.elements
+	? type.elements.map(({ name: insideName }) => insideName).join(', ')
+	: type.name
+       }\` - ${description}`
+    })
+    .join('\n')
+}
+
+function formatSlots(slots) {
+  return slots ? `
+## Slots
+
+| Name          | Description  | Bindings |
+| ------------- | ------------ | -------- |
+${slots.map((slot) => {
+  const { description, bindings, name } = slot;
+  const readableBindings = bindings ? formatBindings(bindings) : '';
+  return [name, description, bindings].join('|');
+}).join('\n')}
+` : '';
+}
+
 export default {
   watch: [
     '../../lib/components/*.component.ts',
@@ -7,61 +64,13 @@ export default {
     '../../lib/components/sources/*.source.ts',
     '../../lib/components/layers/*.layer.ts',
   ],
-  async load(watchedFiles) {
+  load(watchedFiles) {
     return Promise.all(watchedFiles.map(async (file) => {
       let componentInfo = await parse(file);
-
       const type = file.split('/').reverse()[1];
-      const propsMarkdown = componentInfo.props ? `
-## Props
-
-| Name | Description | Type | Required | Default value |
-|------|-------------|------|----------|---------------|
-${componentInfo.props.map((prop) => {
-  return [prop.name, prop?.description?.replaceAll('\n', '  '), prop.type?.name, prop.required || false, prop.defaultValue?.value].join('|')
- }).join('\n')}
-` : '';
-
-      const eventsMarkdown = componentInfo.events ? `
-## Events
-
-| Name | Description | Properties |
-|------|-------------|------------|
-${componentInfo.events.map((event) => {
-  return [event.name, event.description, event.type].join('|')
- }).join('\n')}
-` : '';
-
-
-      const formatBindings = (bindings) => {
-	if (!bindings) {
-	  return ''
-	}
-	return bindings
-	  .map(binding => {
-	    const { name, description, type } = binding
-	    if (!type) {
-	      return ''
-	    }
-	    return `**${name}** \`${
-				type.name === 'union' && type.elements
-					? type.elements.map(({ name: insideName }) => insideName).join(', ')
-					: type.name
-			}\` - ${description}`
-	  })
-	  .join('\n')
-      }
-      const slotsMarkdown = componentInfo.slots ? `
-## Slots
-
-| Name          | Description  | Bindings |
-| ------------- | ------------ | -------- |
-${componentInfo.slots.map((slot) => {
-  const { description, bindings, name } = slot;
-  const readableBindings = bindings ? formatBindings(bindings) : '';
-  return [name, description, bindings].join('|');
-}).join('\n')}
-` : '';
+      const propsMarkdown = formatProps(componentInfo.props);
+      const eventsMarkdown = formatEvents(componentInfo.events);
+      const slotsMarkdown = formatEvents(componentInfo.slots);
 
       return {
         params: {
@@ -86,6 +95,8 @@ ${propsMarkdown}
 ${eventsMarkdown}
 
 ${slotsMarkdown}
+
+## Source
 
 See [source](https://github.com/indoorequal/vue-maplibre-gl/tree/master/${componentInfo.sourceFiles[0]}).
 `
