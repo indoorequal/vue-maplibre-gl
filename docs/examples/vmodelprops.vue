267994cc9a2f34c9b78b148200310cc3cfc86d66
François de Metz <francois@2metz.fr>
2024-05-08 22:01:48 +0200

    Add example on v-model props.

---
+++ docs/examples/vmodelprops.vue
@@ -0,0 +1,39 @@
+// V-model props
+//
+// Support for v-model props on the map
+<template>
+  <div>
+    <mgl-map
+      :map-style="style"
+      v-model:center="center"
+      v-model:zoom="zoom"
+      height="400px"
+    >
+      <mgl-navigation-control />
+    </mgl-map>
+    zoom: {{ zoom }}<br>
+    center: {{ center }}
+  </div>
+</template>
+
+<script setup>
+import {
+  MglMap,
+  MglNavigationControl,
+} from '@indoorequal/vue-maplibre-gl';
+import { ref } from 'vue';
+
+const style = 'https://api.maptiler.com/maps/streets/style.json?key=cQX2iET1gmOW38bedbUh';
+const center = ref([12.550343, 55.665957]);
+const zoom = ref(8);
+</script>
+
+<style lang="scss">
+@import "maplibre-gl/dist/maplibre-gl.css";
+@import "@indoorequal/vue-maplibre-gl/dist/vue-maplibre-gl.css";
+
+body {
+  margin: 0;
+}
+html, body, #app { height: 100%; }
+</style>
