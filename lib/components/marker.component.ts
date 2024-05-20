5fa7f3a3f6741f72f59cd92470189739890150ff
François de Metz <francois@2metz.fr>
2024-05-20 09:25:44 +0200

    Fix the default value comment in the marker component.

--- lib/components/marker.component.ts
+++ lib/components/marker.component.ts
@@ -58,35 +58,35 @@ export default defineComponent({
      */
     offset: [Object, Array] as PropType<PointLike>,
     /**
-     * A string indicating the part of the Marker that should be positioned closest to the coordinate set via Marker#setLngLat. Options are 'center', 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left', and 'bottom-right'. Default Value ts 'center'
+     * A string indicating the part of the Marker that should be positioned closest to the coordinate set via Marker#setLngLat. Options are 'center', 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left', and 'bottom-right'. Default Value 'center'
      */
     anchor: String as PropType<PositionAnchor>,
     /**
-     * The color to use for the default marker if options.element is not provided. The default is light blue. Default Value ts '#3FB1CE'
+     * The color to use for the default marker if options.element is not provided. The default is light blue. Default Value '#3FB1CE'
      */
     color: String as PropType<string>,
     /**
-     * A boolean indicating whether or not a marker is able to be dragged to a new position on the map. Default Value ts false
+     * A boolean indicating whether or not a marker is able to be dragged to a new position on the map. Default Value false
      */
     draggable: Boolean as PropType<boolean>,
     /**
-     * The max number of pixels a user can shift the mouse pointer during a click on the marker for it to be considered a valid click (as opposed to a marker drag). The default is to inherit map's clickTolerance. Default Value ts 0
+     * The max number of pixels a user can shift the mouse pointer during a click on the marker for it to be considered a valid click (as opposed to a marker drag). The default is to inherit map's clickTolerance. Default Value 0
      */
     clickTolerance: Number as PropType<number>,
     /**
-     * The rotation angle of the marker in degrees, relative to its respective rotationAlignment setting. A positive value will rotate the marker clockwise. Default Value ts 0
+     * The rotation angle of the marker in degrees, relative to its respective rotationAlignment setting. A positive value will rotate the marker clockwise. Default Value 0
      */
     rotation: Number as PropType<number>,
     /**
-     * map aligns the Marker's rotation relative to the map, maintaining a bearing as the map rotates. viewport aligns the Marker's rotation relative to the viewport, agnostic to map rotations. auto is equivalent to viewport. Default Value ts 'auto'
+     * map aligns the Marker's rotation relative to the map, maintaining a bearing as the map rotates. viewport aligns the Marker's rotation relative to the viewport, agnostic to map rotations. auto is equivalent to viewport. Default Value 'auto'
      */
     rotationAlignment: String as PropType<"map" | "viewport" | "auto">,
     /**
-     * map aligns the Marker to the plane of the map. viewport aligns the Marker to the plane of the viewport. auto automatically matches the value of rotationAlignment. Default Value ts 'auto'
+     * map aligns the Marker to the plane of the map. viewport aligns the Marker to the plane of the viewport. auto automatically matches the value of rotationAlignment. Default Value 'auto'
      */
     pitchAlignment: String as PropType<"map" | "viewport" | "auto">,
     /**
-     * The scale to use for the default marker if options.element is not provided. The default scale corresponds to a height of 41px and a width of 27px. Default Value ts 1
+     * The scale to use for the default marker if options.element is not provided. The default scale corresponds to a height of 41px and a width of 27px. Default Value 1
      */
     scale: Number as PropType<number>,
   },
