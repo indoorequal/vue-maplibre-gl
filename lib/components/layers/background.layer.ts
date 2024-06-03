684598bae4368d32744033ddcd95d1d1735bfd44
François de Metz <francois@2metz.fr>
2024-06-03 13:27:54 +0200

    Add doc on background layer.

--- lib/components/layers/background.layer.ts
+++ lib/components/layers/background.layer.ts
@@ -10,18 +10,43 @@ import { isLoadedSymbol, mapSymbol } from "@/lib/types";
 import { LayerLib } from "@/lib/lib/layer.lib";
 import { useDisposableLayer } from "@/lib/composable/useDisposableLayer";
 
+/**
+ * Background Layer
+ *
+ * See the [layer style specification](https://maplibre.org/maplibre-style-spec/layers/)
+ */
 export default defineComponent({
   name: "MglBackgroundLayer",
   props: {
+    /**
+     * The id of the layer
+     */
     layerId: {
       type: String as PropType<string>,
       required: true,
     },
+    /**
+     * Arbitrary properties useful to track with the layer, but do not influence rendering. Properties should be prefixed to avoid collisions, like 'maplibre:'.
+     */
     metadata: [Object, Array, String, Number] as PropType<unknown>,
+    /**
+     * The minimum zoom level for the layer. At zoom levels less than the minzoom, the layer will be hidden.
+     */
     minzoom: Number as PropType<number>,
+    /**
+     * The maximum zoom level for the layer. At zoom levels equal to or greater than the maxzoom, the layer will be hidden.
+     */
     maxzoom: Number as PropType<number>,
     before: String as PropType<string>,
+    /**
+     * Layout properties for the layer.
+     * See https://maplibre.org/maplibre-style-spec/layers/#background
+     */
     layout: Object as PropType<BackgroundLayerSpecification["layout"]>,
+    /**
+     * Default paint properties for this layer.
+     * See https://maplibre.org/maplibre-style-spec/layers/#background
+     */
     paint: Object as PropType<BackgroundLayerSpecification["paint"]>,
   },
   emits: [...LayerLib.SHARED.emits],
