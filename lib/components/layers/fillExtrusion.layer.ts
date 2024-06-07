c517b334a352135cf6fec7519d560d2757c6d135
François de Metz <francois@2metz.fr>
2024-06-07 15:36:53 +0200

    Add class doc on layers components.

--- lib/components/layers/fillExtrusion.layer.ts
+++ lib/components/layers/fillExtrusion.layer.ts
@@ -3,6 +3,11 @@ import { defineComponent } from "vue";
 import { layerProps, LAYER_EVENTS } from "@/lib/lib/layer.lib";
 import { useLayer } from "@/lib/composable/useLayer";
 
+/**
+ * Fill Extrusion Layer
+ *
+ * See the [layer style specification](https://maplibre.org/maplibre-style-spec/layers/)
+ */
 export default defineComponent({
   name: "MglFillExtrusionLayer",
   props: { ...layerProps<FillExtrusionLayerSpecification>() },
--- lib/components/layers/hillshade.layer.ts
+++ lib/components/layers/hillshade.layer.ts
@@ -3,6 +3,11 @@ import { defineComponent } from "vue";
 import { layerProps, LAYER_EVENTS } from "@/lib/lib/layer.lib";
 import { useLayer } from "@/lib/composable/useLayer";
 
+/**
+ * Hill shade Layer
+ *
+ * See the [layer style specification](https://maplibre.org/maplibre-style-spec/layers/)
+ */
 export default defineComponent({
   name: "MglHillshadeLayer",
   props: { ...layerProps<HillshadeLayerSpecification>() },
