998e2da2647eb0288770fb826dc4b0dd7389e674
François de Metz <francois@2metz.fr>
2024-10-13 10:04:52 +0200

    Create a custom control example.

---
+++ docs/examples/custom-control.vue
@@ -0,0 +1,75 @@
+// Custom Control
+//
+// A custom control with a button
+<template>
+  <mgl-map
+    :map-style="style"
+    :center="center"
+    :zoom="zoom"
+    height="500px"
+  >
+    <mgl-custom-control
+      v-if="showCustomControl"
+      :position="controlPosition"
+    >
+      <button
+        class="maplibregl-ctrl-icon"
+        style="color: deepskyblue"
+        @click="onClick"
+      >
+        <svg>
+          <path
+            :d="mdiCursorDefaultClick"
+            fill="currentColor"
+            />
+        </svg>
+      </button>
+    </mgl-custom-control>
+  </mgl-map>
+  <div>
+    <input type="checkbox" v-model="showCustomControl" id="showcustom">
+    <label for="showcustom">Show Custom Control</label>
+  </div>
+  <div>
+    Control position:
+    <br />
+    <input type="radio" id="one" value="top-left" v-model="controlPosition"/>
+    <label for="one">top-left</label>
+    <br/>
+    <input type="radio" id="tw0" value="top-right" v-model="controlPosition"/>
+    <label for="tw0">top-right</label>
+    <br/>
+    <input type="radio" id="three" value="bottom-left" v-model="controlPosition"/>
+    <label for="three">bottom-left</label>
+    <br/>
+    <input type="radio" id="four" value="bottom-right" v-model="controlPosition"/>
+    <label for="four">bottom-right</label>
+    <br/>
+    <span>Style Position: {{ controlPosition }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  MglMap,
+  MglCustomControl,
+  Position,
+} from '@indoorequal/vue-maplibre-gl';
+import { ref } from 'vue';
+import { mdiCursorDefaultClick } from '@mdi/js';
+
+const style = 'https://api.maptiler.com/maps/streets-v2/style.json?key=3YeFnghdqUJJpIvlgLti';
+const center = [ -122.483696, 37.833818 ];
+const zoom = 15;
+
+const showCustomControl = ref(true);
+const controlPosition = ref(Position.TOP_LEFT);
+
+const onClick = function() {
+  console.log('button clicked !')
+}
+</script>
+
+<style lang="scss">
+@import "maplibre-gl/dist/maplibre-gl.css";
+</style>
--- docs/examples/playground.vue
+++ docs/examples/playground.vue
@@ -7,7 +7,6 @@
     <mgl-map
       v-if="showMap"
       height="400px"
-      ref="map"
       :center="center"
       :zoom="zoom"
       :attribution-control="false"
@@ -20,36 +19,11 @@
       <mgl-navigation-control/>
       <mgl-scale-control/>
       <mgl-geolocate-control/>
-      <mgl-custom-control v-if="showCustomControl" :position="controlPosition">
-        <button class="maplibregl-ctrl-icon" style="color: deepskyblue">
-          <svg><path :d="buttonIcon" fill="currentColor" /></svg>
-        </button>
-      </mgl-custom-control>
-      <mgl-marker :coordinates="markerCoordinates" color="#cc0000" :scale="0.5"/>
     </mgl-map>
   </div>
   <div style="margin-bottom: 20px">Version: {{ mapVersion }}</div>
   Loaded Count: {{ loaded }}<br>
   Is Zooming: {{ isZooming }}<br>
-  <div>
-    <input type="radio" id="one" value="top-left" v-model="controlPosition"/>
-    <label for="one">top-left</label>
-    <br/>
-    <input type="radio" id="tw0" value="top-right" v-model="controlPosition"/>
-    <label for="tw0">top-right</label>
-    <br/>
-    <input type="radio" id="three" value="bottom-left" v-model="controlPosition"/>
-    <label for="three">bottom-left</label>
-    <br/>
-    <input type="radio" id="four" value="bottom-right" v-model="controlPosition"/>
-    <label for="four">bottom-right</label>
-    <br/>
-    <span>Style Position: {{ controlPosition }}</span>
-  </div>
-  <div>
-    <input type="checkbox" v-model="showCustomControl" id="showcustom">
-    <label for="showcustom">Show Custom Control</label>
-  </div>
   <div>
     <input type="checkbox" v-model="showMap" id="showmap">
     <label for="showmap">Show Map</label>
@@ -58,11 +32,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, watch } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 import {
   MglDefaults,
   MglEvent,
-  Position,
   useMap,
   MglMap,
   MglFullscreenControl,
@@ -70,10 +43,7 @@ import {
   MglNavigationControl,
   MglScaleControl,
   MglGeolocateControl,
-  MglCustomControl,
-  MglMarker,
 } from '@indoorequal/vue-maplibre-gl';
-import { mdiCursorDefaultClick } from '@mdi/js';
 import { LngLatLike, MapLayerMouseEvent } from 'maplibre-gl';
 
 MglDefaults.style = 'https://api.maptiler.com/maps/streets-v2/style.json?key=3YeFnghdqUJJpIvlgLti';
@@ -81,22 +51,20 @@ MglDefaults.style = 'https://api.maptiler.com/maps/streets-v2/style.json?key=3Ye
 export default defineComponent({
   name      : 'App',
   components: {
-    MglMarker, MglCustomControl,
-    MglGeolocateControl, MglScaleControl, MglNavigationControl, MglAttributionControl, MglFullscreenControl, MglMap
+    MglGeolocateControl,
+    MglScaleControl,
+    MglNavigationControl,
+    MglAttributionControl,
+    MglFullscreenControl,
+    MglMap
   },
   setup() {
     const map               = useMap(),
           mapVersion        = ref<string>(),
-          showCustomControl = ref(true),
-          loaded            = ref(0),
-          markerCoordinates = ref<LngLatLike>([ 13.377507, 52.516267 ]);
+          loaded            = ref(0);
 
     watch(() => map.isLoaded, () => (console.log('IS LOADED', map)), { immediate: true });
     watch(() => map.isMounted, (v: boolean) => (console.log('IS MOUNTED', v)), { immediate: true });
-
-    onMounted(() => {
-      setTimeout(() => (markerCoordinates.value = [ 13.377507, 42.516267 ]), 5000);
-    });
 
     function onLoad(e: MglEvent) {
       loaded.value++;
@@ -105,13 +73,11 @@ export default defineComponent({
     }
 
     return {
-      showCustomControl, loaded, map, mapVersion, markerCoordinates, onLoad,
+      loaded, map, mapVersion, onLoad,
       isZooming                 : ref(false),
-      controlPosition           : ref(Position.TOP_LEFT),
       showMap                   : ref(true),
       center                    : [ -122.483696, 37.833818 ] as LngLatLike,
       zoom                      : 15,
-      buttonIcon                : mdiCursorDefaultClick,
     };
   }
 });
