ce5321d2ca95396de28da154a45d4c9c0b216cb6
François de Metz <francois@2metz.fr>
2025-01-03 21:09:00 +0100

    Add globe example.

---
+++ docs/examples/globe.vue
@@ -0,0 +1,40 @@
+// Display a globe
+//
+// Display a globe with a vector map.
+<template>
+  <mgl-map
+    :map-style="style"
+    :center="center"
+    :zoom="zoom"
+    height="500px"
+  >
+    <mgl-navigation-control />
+  </mgl-map>
+</template>
+
+<script setup>
+import {
+  MglMap,
+  MglNavigationControl,
+  useMap,
+} from '@indoorequal/vue-maplibre-gl';
+import { watch } from 'vue';
+
+const style = 'https://api.maptiler.com/maps/streets-v2/style.json?key=3YeFnghdqUJJpIvlgLti';
+const center = [12.550343, 55.665957];
+const zoom = 1;
+
+const map = useMap();
+watch(
+  () => map.isLoaded,
+  () => {
+    map.map.setProjection({
+        type: 'globe',
+    });
+ }
+);
+</script>
+
+<style lang="scss">
+@import "maplibre-gl/dist/maplibre-gl.css";
+</style>
