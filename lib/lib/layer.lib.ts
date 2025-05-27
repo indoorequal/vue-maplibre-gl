bcdf39a7db8ca926203faf31e390a1b247e4993b
Jason Baker <jason.th.baker@gmail.com>
2025-05-27 16:46:36 -0500

    added some cleanup

--- lib/lib/layer.lib.ts
+++ lib/lib/layer.lib.ts
@@ -12,7 +12,7 @@ import type {
   FilterSpecification,
   BackgroundLayerSpecification,
 } from "maplibre-gl";
-import { type PropType, type VNode, type ComponentPropsOptions } from "vue";
+import { type PropType, type VNode } from "vue";
 
 export type LayersWithSource =
   | FillLayerSpecification
@@ -26,15 +26,17 @@ export type LayersWithSource =
 
 export type LayersWithoutSource = BackgroundLayerSpecification;
 
-export type LayerProps<T extends LayersWithSource> = Omit<
-  T,
-  "source" | "source-layer" | "id" | "type"
-> & {
+export type AddedProperties = {
   layerId?: string;
   sourceLayer?: string;
   source?: string;
   before?: string;
-};
+}
+
+export type LayerProps<T extends LayersWithSource> = Omit<
+  (AddedProperties & T),
+  "source-layer" | "id" | "type"
+>;
 
 export type LayerEventType = keyof MapLayerEventType
 
@@ -54,53 +56,65 @@ export const LAYER_EVENTS: Array<LayerEventType> = [
   "touchcancel",
 ] as const;
 
+export type LayerPropTypeNaive<T extends LayersWithSource> = {
+  [K in keyof LayerProps<T>]: { type: PropType<LayerProps<T>[K]>, required?: boolean | undefined }
+}
+
+export type LayerPropTypeAdded = {layerId: {type: PropType<string>}, sourceLayer: { type: PropType<string>}, before: { type: PropType<string>}, source: { type: PropType<string>}}
+
+export type LayerPropType<T extends LayersWithSource> = LayerPropTypeNaive<T> & LayerPropTypeAdded
+
 export function layerProps<
   T extends LayersWithSource,
->(): ComponentPropsOptions<LayerProps<T>> {
+>() {
   return {
     /**
      * The id of the layer
      */
     layerId: {
-      type: String as PropType<string>,
+      type: String as PropType<T['id']>,
       required: true,
     },
-    source: String as PropType<string>,
+    source: {
+      type: String as PropType<T['source']>
+    },
     /**
      * Arbitrary properties useful to track with the layer, but do not influence rendering. Properties should be prefixed to avoid collisions, like 'maplibre:'.
      */
-    metadata: [Object, Array, String, Number] as PropType<unknown>,
+    metadata: {
+      type: [Object, Array, String, Number] as PropType<T['metadata']>
+    },
     /**
      * Layer to use from a vector tile source. Required for vector tile sources; prohibited for all other source types, including GeoJSON sources.
      */
-    sourceLayer: String as PropType<string>,
+    sourceLayer: {type: String as PropType<T['source-layer']>},
     /**
      * The minimum zoom level for the layer. At zoom levels less than the minzoom, the layer will be hidden.
      */
-    minzoom: Number as PropType<number>,
+    minzoom: { type: Number as PropType<T['minzoom']> },
     /**
      * The maximum zoom level for the layer. At zoom levels equal to or greater than the maxzoom, the layer will be hidden.
      */
-    maxzoom: Number as PropType<number>,
+    maxzoom: { type: Number as PropType<T['maxzoom']> },
     /**
      * A expression specifying conditions on source features. Only features that match the filter are displayed. Zoom expressions in filters are only evaluated at integer zoom levels. The feature-state expression is not supported in filter expressions.
      */
-    filter: Object as PropType<FilterSpecification>,
+    filter: { type: Object as PropType<T['filter']> },
     /**
      * The ID of an existing layer to insert the new layer before, resulting in the new layer appearing visually beneath the existing layer. If this argument is not specified, the layer will be appended to the end of the layers array and appear visually above all other layers.
      */
-    before: String as PropType<string>,
+    before: { type: String as PropType<string> },
     /**
      * Layout properties for the layer.
      * See https://maplibre.org/maplibre-style-spec/layers/
      */
-    layout: Object as PropType<T["layout"]>,
+    layout: { type: Object as PropType<T["layout"]> },
     /**
      * Default paint properties for this layer.
      * See https://maplibre.org/maplibre-style-spec/layers/
      */
-    paint: Object as PropType<T["paint"]>,
-  } as ComponentPropsOptions<LayerProps<T>>;
+    paint: { type: Object as PropType<T["paint"]> },
+  };
 }
 
 export function genLayerOpts<T extends LayersWithSource>(
@@ -112,7 +126,7 @@ export function genLayerOpts<T extends LayersWithSource>(
   const opts = {
     id,
     type,
-    source: props.source || source,
+    source: source,
     metadata: props.metadata,
     minzoom: props.minzoom,
     maxzoom: props.maxzoom,
